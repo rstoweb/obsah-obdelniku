8c3d8f36ab39bad8a6ce5a0857a8e4e195cf40cd
RS <ramsh@seznam.cz>
2024-10-01 12:14:28 +0200

    Ošetření chyby

--- obsah_obd.py
+++ obsah_obd.py
@@ -1,4 +1,8 @@
 a = int(input("Zadej stranu a: "))
 b = int(input("Zadej stranu b: "))
-obsah = a * b
-print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+if a and b > 0:
+    obsah = a * b
+    print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+else:
+    print("Chyba")
+    
