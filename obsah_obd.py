bc6f61f3b4e0bb0b867650cd83e254133e57ce9d
RS <ramsh@seznam.cz>
2024-10-01 12:19:59 +0200

    Vytvořena funkce obsah_obdelniku()

--- obsah_obd.py
+++ obsah_obd.py
@@ -1,8 +1,10 @@
-a = int(input("Zadej stranu a: "))
-b = int(input("Zadej stranu b: "))
-if a and b > 0:
-    obsah = a * b
-    print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
-else:
-    print("Chyba")
-    
+def obsah_obdelniku():    
+    a = int(input("Zadej stranu a: "))
+    b = int(input("Zadej stranu b: "))
+    if a > 0 and b > 0:
+        obsah = a * b
+        print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+    else:
+        print("Chyba")
+
+obsah_obdelniku()
